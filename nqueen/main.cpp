2a6385ab3f453cfd4348c72c6982391f9a903546
Komuro Sunao <konbu.komuro@gmail.com>
2010-03-10 11:26:21 +0900

    updated nqueens.
    * add options: --help, --all

--- nqueen/main.cpp
+++ nqueen/main.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
+#include <boost/format.hpp>
+#include <boost/tuple/tuple.hpp>
 #include <boost/program_options.hpp>
 
 #include "nqueen.hpp"
 
 namespace options = boost::program_options;
-
-void parse_options (options::variables_map& vmap, int argc, char *argv[])
+typedef boost::tuple<options::options_description * const, options::variables_map * const> Option;
+Option parse_options (int argc, char *argv[])
 {
-    options::options_description opt("hidden option");
-    options::positional_options_description pos_opt;
+    static options::variables_map vmap;
+    static options::options_description opt("options");
     opt.add_options()
+        ("help,h", "print this help")
+        ("all,a", "print all solutions");
+
+    options::options_description hopt("hidden options");
+    hopt.add_options()
         ("input", options::value<int>()->default_value(8), "N");
-    pos_opt.add("input", -1);
-    store(options::command_line_parser(argc, argv).options(opt).positional(pos_opt).run(), vmap);
+
+    options::positional_options_description pos_hopt;
+    pos_hopt.add("input", -1);
+
+    options::command_line_parser parser(argc, argv);
+    options::options_description all_opt("all options");
+
+    hopt.add(opt);
+    parser.options(hopt).positional(pos_hopt);
+    options::store(parser.run(), vmap);
+
+    return boost::make_tuple(&opt, &vmap);
 }
 
 int main (int argc, char *argv[])
 {
-    options::variables_map vmap;
-    parse_options(vmap, argc, argv);
+    options::options_description * opt;
+    options::variables_map * vmap;
+    boost::tie(opt, vmap) = parse_options(argc, argv);
 
-    int N = vmap["input"].as<int>();
+    if (vmap->count("help")) {
+        std::cout <<
+            (boost::format("%1% [options] [N = 8]\n") % argv[0]) <<
+            *opt << std::endl;
+        exit(0);
+    }
+
+    int N = (*vmap)["input"].as<int>();
 
     Board board(N);
     const Solution& sol = board.solve();
@@ -28,7 +53,8 @@ int main (int argc, char *argv[])
         "queens: " << N << "\n" <<
         "solutions: " << sol.size() << "\n";
 
-    board.print_solution(sol);
+    if (vmap->count("all"))
+        board.print_solution(sol);
 
     return 0;
 }
